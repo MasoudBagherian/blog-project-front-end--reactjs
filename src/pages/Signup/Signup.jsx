0d1dcc68fa75ee0bb171b35325749a3c572aebf9
Pouria Baqerian <pouriacss.69@gmail.com>
2021-09-02 03:24:44 +0430

    add Signup component

---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignupForm from '../../components/forms/Signup/SignupForm';
+import Main from '../../hoc/Main';
+const Signup = () => {
+  return (
+      
+    <Main>
+      <SignupForm/>
+    </Main>
+  )
+}
+
+export default Signup;
